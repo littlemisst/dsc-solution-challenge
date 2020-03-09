316348d12d4132f7fc07bae00cf601b95484ddd3
fmg050499 <francismathew2017@gmail.com>
2020-03-09 09:36:55 +0800

    chart with database

--- lib/pages/healthChart.dart
+++ lib/pages/healthChart.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:me_daily/model/feeling.dart';
@@ -10,19 +11,14 @@ class HealthChart extends StatefulWidget {
 class _HealthChartState extends State<HealthChart> {
   List<charts.Series<Health, String>> _healthData;
 
-  _generateData() {
-    final data = [
-      Health('Very Good', 1),
-      Health('Good', 5),
-      Health('Nuetral', 5),
-      Health('Bad', 10),
-      Health('Very Bad', 5),
-    ];
+  List<Health> myData;
+
+  _generateData(myData) {
     _healthData.add(charts.Series(
       id: 'Health',
-      domainFn: (Health health, _) => health.feeling,
+      domainFn: (Health health, _) => health.feeling.toString(),
       measureFn: (Health health, _) => health.count,
-      data: data,
+      data: myData,
       labelAccessorFn: (Health row, _) => '${row.feeling}: ${row.count}',
     ));
   }
@@ -30,15 +26,31 @@ class _HealthChartState extends State<HealthChart> {
   @override
   void initState() {
     _healthData = List<charts.Series<Health, String>>();
-    _generateData();
     super.initState();
   }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildChart(context, health) {
+    myData = health;
+    _generateData(myData);
     return charts.PieChart(_healthData,
         animate: true,
         defaultRenderer: charts.ArcRendererConfig(
             arcWidth: 60, arcRendererDecorators: [charts.ArcLabelDecorator()]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('mockdata').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading ...');
+        } else {
+          List<Health> health = snapshot.data.documents
+              .map((documentSnapshot) => Health.fromMap(documentSnapshot.data))
+              .toList();
+          return _buildChart(context, health);
+        }
+      },
+    );
   }
 }
