d0d65ad88e06edd01cbeedbd51739455448487e6
teofy <teofyrbns@gmail.com>
2020-03-19 05:25:27 +0800

    added hours of sleep expansion tile. page to be refactored when done

--- lib/updated-pages/dailyLogs/basicQuestions.dart
+++ lib/updated-pages/dailyLogs/basicQuestions.dart
@@ -11,6 +11,7 @@ class _BasicQuestionsPageState extends State<BasicQuestionsPage> {
   String _food;
   String _drink;
   String _exercise;
+  int _hoursOfSleep = 0;
 
   void _setFood(value) {
     setState(() {
@@ -33,6 +34,23 @@ class _BasicQuestionsPageState extends State<BasicQuestionsPage> {
     });
   }
 
+   void _incrementSleepHours() {
+    setState(() {
+     _hoursOfSleep++;
+      //insert pag butang sa db;
+    });
+  }
+
+  void _decrementSleepHours() {
+    setState(() {
+     _hoursOfSleep--;
+      //insert pag butang sa db;
+    });
+    if (_hoursOfSleep < 0) {
+      _hoursOfSleep = 0;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -50,7 +68,7 @@ class _BasicQuestionsPageState extends State<BasicQuestionsPage> {
         elevation: 0,
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width/10, MediaQuery.of(context).size.height/10, MediaQuery.of(context).size.width/10, 0),
+        padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width/10, 10, MediaQuery.of(context).size.width/10, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
@@ -59,8 +77,21 @@ class _BasicQuestionsPageState extends State<BasicQuestionsPage> {
             ExpandableCard(ImageIcon(AssetImage("images/food.png"), color: Colors.pink[100],),'FOOD', Strings.food, _food, (value) => _setFood(value)),
             SizedBox(height: 15),
             ExpandableCard(ImageIcon(AssetImage("images/drink.png"), color: Colors.pink[100]),'DRINK', Strings.drink, _drink, (value) => _setDrink(value)),
+            SizedBox(height: 15),
+            ExpandableCard(ImageIcon(AssetImage("images/exercise.png"), color: Colors.pink[100]),'EXERCISE', Strings.exercise, _exercise, (value) => _setExercise(value)),
              SizedBox(height: 15),
-            ExpandableCard(ImageIcon(AssetImage("images/exercise.png"), color: Colors.pink[100]),'EXERCISE', Strings.exercise, _exercise, (value) => _setExercise(value))
+            ExpansionTile(leading: ImageIcon(AssetImage("images/sleep.png"), color: Colors.pink[100]),
+                title: Text('SLEEP'),
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Text('Number of hours: ', style: TextStyle(fontSize: 15)),
+                    SizedBox(width: 10),
+                    Text('$_hoursOfSleep', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                    FlatButton(child: ImageIcon(AssetImage("images/add.png"), color: Colors.pink[100],), color: Colors.white, shape: CircleBorder(side: BorderSide.none),onPressed: _incrementSleepHours),
+                    FlatButton(child: ImageIcon(AssetImage("images/minus.png"), color: Colors.pink[100],), color: Colors.white, shape: CircleBorder(side: BorderSide.none),onPressed: _decrementSleepHours)
+                  ],)
+                ],
+              )
           ],
         ),
       )
