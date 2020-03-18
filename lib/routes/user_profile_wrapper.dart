fddd2cd6b474b1e965d3669a54767eb083839929
fmg050499 <francismathew2017@gmail.com>
2020-03-18 19:23:39 +0800

    fix height and weight fields

--- lib/routes/user_profile_wrapper.dart
+++ lib/routes/user_profile_wrapper.dart
@@ -4,13 +4,11 @@ import 'package:me_daily/pages/update_profile.dart';
 import 'package:provider/provider.dart';
 import 'package:me_daily/model/profile.dart';
 
-
 class UserProfileWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   final profile = Provider.of<Profile>(context);
-  
-    
+    final profile = Provider.of<Profile>(context);
+
     return profile == null ? UpdateProfile() : UserProfileSummary();
   }
 }
