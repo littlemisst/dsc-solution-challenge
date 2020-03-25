ebc19f8b8c6009e0c852a4ff2b9c8de0db259ed2
fmg050499 <francismathew2017@gmail.com>
2020-03-25 19:28:46 +0800

    edited pages form sending details

--- lib/updated-pages/sendDetails/dailyLogsPage.dart
+++ lib/updated-pages/sendDetails/dailyLogsPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:me_daily/model/logs.dart';
+import 'package:me_daily/model/user.dart';
+import 'package:me_daily/services/firestore_service.dart';
+import 'package:provider/provider.dart';
 
 class DailyLogsPage extends StatefulWidget {
   @override
@@ -8,8 +12,31 @@ class DailyLogsPage extends StatefulWidget {
 class _DailyLogsPageState extends State<DailyLogsPage> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     return Container(
-      child: Text('Page 2'),
+      child: StreamBuilder<List<DailyLog>>(
+        stream: FirestoreService(uid: user.uid).logs,
+        builder: (context, snapshots) {
+          if (snapshots.hasData) {
+            return ListView.builder(
+                itemCount: snapshots.data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                    child: ListTile(
+                      leading: Icon(Icons.delete),
+                      title: TextFormField(
+                        initialValue: snapshots.data[index].feeling.toString(),
+                      ),
+                      subtitle:
+                          Text(snapshots.data[index].logCreated.toString()),
+                    ),
+                  );
+                });
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
