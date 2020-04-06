ee4cd90607170cff0ae4ca63507c6f88017b44e9
fmg050499 <francismathew2017@gmail.com>
2020-04-07 03:41:56 +0800

    view fullscreen image from grid

--- lib/updated-pages/photos/gallery_grid.dart
+++ lib/updated-pages/photos/gallery_grid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:me_daily/model/photo.dart';
+import 'package:me_daily/updated-pages/photos/full_screen.dart';
 import 'package:provider/provider.dart';
 
 class GalleryGrid extends StatefulWidget {
@@ -11,8 +12,8 @@ class _GalleryGridState extends State<GalleryGrid> {
   @override
   Widget build(BuildContext context) {
     List<Photo> photos = Provider.of<List<Photo>>(context);
-    
-    if(photos == null){
+
+    if (photos == null) {
       photos = [];
     }
 
@@ -29,12 +30,24 @@ class _GalleryGridState extends State<GalleryGrid> {
 
 Widget _buildPhotoListItem(context, document) {
   return Container(
-    child: GridTile(
-      footer: Text(document.fileName),
-      child: Image.network(
-        document.downloadURL.toString(),
-        fit: BoxFit.cover,
-      ),
-    ),
+    child: GestureDetector(
+        child: GridTile(
+          footer: Text(document.fileName),
+          child: Hero(
+            tag: document.fileName,
+            child: Image.network(
+              document.downloadURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return FullScreen(
+              imageURL: document.downloadURL,
+              tag: document.fileName,
+            );
+          }));
+        }),
   );
 }
