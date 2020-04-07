eae2d1758b78d30e58fd55febcb3fc308429f517
teofy <teofyrbns@gmail.com>
2020-04-07 18:15:37 +0800

    fixed checkbox

--- lib/updated-pages/dailyLogs/basicQuestionsLog.dart
+++ lib/updated-pages/dailyLogs/basicQuestionsLog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:me_daily/common-widgets/buildGrid.dart';
-import 'package:me_daily/common-widgets/radioListViewBuilderWidget.dart';
 import 'package:me_daily/constants/strings.dart';
 import 'package:me_daily/model/logs.dart';
-import 'package:me_daily/updated-pages/dailyLogs/checkBoxWidget.dart';
+import 'package:me_daily/updated-pages/dailyLogs/checkBoxContainer.dart';
 import 'package:me_daily/updated-pages/dailyLogs/stepperWidget.dart';
 
 class BasicQuestionsLogPage extends StatefulWidget {
@@ -20,15 +18,13 @@ class _BasicQuestionsLogPageState extends State<BasicQuestionsLogPage> {
   List<String> _drink = Strings.drink;
   List<bool> _drinkValues = List<bool>();
   List<bool> _foodValues = List<bool>();
-  
-  // Map<String, bool> values = {
-  //   'foo' : true,
-  //   'bar' : false
-  // };
+
 
   @override
   void initState() {
     super.initState();
+    widget.entry.food = List<String>();
+    widget.entry.drink = List<String>();
     setState(() {
       for(int i=0; i < _food.length; i++){
         _foodValues.add(false);
@@ -39,72 +35,29 @@ class _BasicQuestionsLogPageState extends State<BasicQuestionsLogPage> {
     });
   }
 
-  void _itemChange(List values, bool value, int index) {
-    setState(() {
-      values[index] = value;
-    });
-  }
-
-  Widget _buildCheckBoxList() {
-    print(_foodValues);
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      child: GridView.builder(
-      shrinkWrap: true,
-      itemCount: _food.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 6) ,
-      itemBuilder: (BuildContext context, int index) {
-        return LogCheckBox(_food[index], _foodValues[index], (value) {
-          _itemChange(_foodValues, value, index);
-        });
-      },
-      )
-    );
-  }
-//   }
-
-Widget _buildCheckBoxDrinkList() {
-    return Container(
-       child: Align (
-        child: Material(
-          color: Colors.white,
-          elevation: 1,
-          borderRadius: BorderRadius.circular(10),
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: _drink.length,
-            itemBuilder: (BuildContext context, int index) {
-              return LogCheckBox(_drink[index], _drinkValues[index], (value) {
-                _itemChange(_drinkValues, value, index);
-              });
-            },
-          )
-        )
-       )
-    );
-  }
-
 
   List<Step> get _steps => [
     Step(
       title: Text('Eating'),
-      content: _buildCheckBoxList()),
+      content: CheckBoxGrid(_food, _foodValues, widget.entry.food)),
       Step(
       title: Text('Drinking'),
-      content: _buildCheckBoxDrinkList())
+      content: CheckBoxGrid(_drink, _drinkValues, widget.entry.drink))
 
   ];
 
 
   void _onStepContinue() {
     if (_currentStep >= _steps.length) return;
+    if (widget.entry.food.isNotEmpty || widget.entry.drink.isNotEmpty) {
       setState(() {
-        _currentStep += 1;
+          _currentStep ++;
       });
+    }
   }
 
   void _onStepCancel() {
-    if (_currentStep <= 0) return;
+    if (_currentStep < 0) return;
       setState(() {
         _currentStep -= 1;
       });
---
+++ lib/updated-pages/dailyLogs/checkBoxContainer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:me_daily/updated-pages/dailyLogs/checkBoxWidget.dart';
+
+class CheckBoxGrid extends StatefulWidget {
+  CheckBoxGrid(this.items, this.itemValues, this.taskAdded);
+  final List<String> items;
+  final List<bool> itemValues;
+  final List<String> taskAdded;
+  // final List<String> model;
+
+  @override
+  _CheckBoxGridState createState() => _CheckBoxGridState();
+}
+
+
+class _CheckBoxGridState extends State<CheckBoxGrid> {
+  void _itemChange(List items, List values, bool value, int index) {
+    setState(() {
+      values[index] = value;
+      if (value == true) {
+        widget.taskAdded.add(items[index]);
+      } else {
+        widget.taskAdded.remove(items[index]);
+      }
+      //  model = widget.taskAdded;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    print(widget.taskAdded);
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Material(
+          color: Colors.white,
+          elevation: 1,
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: GridView.builder(
+            shrinkWrap: true,
+            itemCount: widget.items.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 6) ,
+            itemBuilder: (BuildContext context, int index) {
+              return LogCheckBox(widget.items[index], widget.itemValues[index], (value) {
+                _itemChange(widget.items, widget.itemValues, value, index);
+              });
+            },
+          ))
+      )
+    );
+  }
+}
