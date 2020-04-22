ca83bb84cf9916a7095de39fd5a532fe50796543
fmg050499 <francismathew2017@gmail.com>
2020-04-22 16:07:46 +0800

    fix edit profile

--- lib/updated-pages/profile/user_profile_summary.dart
+++ lib/updated-pages/profile/user_profile_summary.dart
@@ -12,130 +12,147 @@ class UserProfileSummary extends StatefulWidget {
 }
 
 class _UserProfileSummaryState extends State<UserProfileSummary> {
-
   @override
   Widget build(BuildContext context) {
     Profile _currentProfile = Provider.of<Profile>(context);
-    final bmi = _currentProfile.weight / (_currentProfile.height * _currentProfile.height) ;
+    final bmi = _currentProfile.weight /
+        (_currentProfile.height * _currentProfile.height);
     var formatter = new DateFormat('MM-dd-yyyy');
     String formatBirthDate = formatter.format(_currentProfile.birthDate);
-    var difference = DateTime.now().difference(_currentProfile.birthDate).inDays;
-    var age = (difference/365).floor().toString();
+    var difference =
+        DateTime.now().difference(_currentProfile.birthDate).inDays;
+    var age = (difference / 365).floor().toString();
 
     if (difference < 0) {
       age = '0';
     }
 
     String bmiCategory;
-    
+
     if (bmi <= 0) {
       bmiCategory = 'Invalid';
     } else if (bmi < 18.5) {
       bmiCategory = 'Underweight';
-    } else if (bmi >= 18.5 && bmi <= 24.9 ){
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
       bmiCategory = 'Normal';
-    } else if (bmi >= 25 && bmi <= 29.9){
+    } else if (bmi >= 25 && bmi <= 29.9) {
       bmiCategory = 'Overweight';
     } else if (bmi >= 30) {
       bmiCategory = 'Obese';
     }
-    
-    return SingleChildScrollView(
-      padding: EdgeInsets.all(20.0),
-      child: Column(children: <Widget>[
-        Container(
-          width: 1000,
-          height: 175,
-          color: Theme.of(context).primaryColor,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+        onPressed: () =>
+            Navigator.popAndPushNamed(context, Strings.editProfilePageRoute),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: <Widget>[
+          Container(
+              width: 1000,
+              height: 175,
+              color: Theme.of(context).primaryColor,
+              child: Column(
                 children: <Widget>[
-                  SizedBox(width: 50),
-                  _currentProfile.downloadUrl == null
-                      ? CircleAvatar(
-                          radius: 45.0, backgroundColor: Colors.grey[350])
-                      : CircleAvatar(
-                          radius: 45.0,
-                          backgroundImage: NetworkImage(_currentProfile.downloadUrl, scale: 50.0),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(width: 50),
+                      _currentProfile.downloadUrl == null
+                          ? CircleAvatar(
+                              radius: 45.0, backgroundColor: Colors.grey[350])
+                          : CircleAvatar(
+                              radius: 45.0,
+                              backgroundImage: NetworkImage(
+                                  _currentProfile.downloadUrl,
+                                  scale: 50.0),
+                            ),
+                      IconButton(
+                        tooltip: 'Change Profile Photo',
+                        icon: Icon(Icons.camera_alt, color: Colors.white),
+                        iconSize: 20,
+                        onPressed: () {
+                          Navigator.popAndPushNamed(
+                              context, Strings.editProfilePhotoPageRoute);
+                        },
                       ),
-                      IconButton(tooltip: 'Change Profile Photo' , icon: Icon(Icons.edit, color: Colors.white), iconSize: 20,
-                      onPressed: () {Navigator.popAndPushNamed(context, Strings.editProfilePhotoPageRoute);},
-                  ),   
+                    ],
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(_currentProfile.name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold)),
                 ],
-              ),
-              SizedBox(height: 12.0),
-              Text(_currentProfile.name,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold)),
-              ],
-          )),
-        SizedBox(height: 20.0),
-        TextWidget(text: 'Address: '),
-        Text(_currentProfile.address, style: TextStyle(fontSize: 18.0)),
-        Divider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextWidget(text: 'Age: '),
-            Text(age, style: TextStyle(fontSize: 18.0)),
-            SizedBox(width: 20.0),
-            TextWidget(text: 'Birthdate: '),
-            Text(formatBirthDate, style: TextStyle(fontSize: 18.0)),
-          ],
-        ),
-        Divider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextWidget(text: 'Gender :'),
-            SizedBox(width: 5.0),
-            Text(_currentProfile.gender, style: TextStyle(fontSize: 18.0)),
-            SizedBox(width: 20.0),
-           TextWidget(text: 'Civil Status:'),
-            SizedBox(width: 5.0),
-            Text(_currentProfile.civilStatus, style: TextStyle(fontSize: 18.0)),
-          ],
-        ),
-        Divider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          TextWidget(text: 'Blood Type:'),
-          SizedBox(width: 5.0),
-          Text(_currentProfile.bloodType, style: TextStyle(fontSize: 18.0)),
-        ]),
-        Divider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          TextWidget(text: 'Height:'),
-          SizedBox(width: 5.0),
-          Text(_currentProfile.height.toString() + ' m', style: TextStyle(fontSize: 18.0)),
-           SizedBox(width: 20.0),
-          TextWidget(text: 'Weight:'),
-          SizedBox(width: 5.0),
-          Text(_currentProfile.weight.toString() + ' kg',style: TextStyle(fontSize: 18.0)),
-        ]),
-        Divider(),
+              )),
+          SizedBox(height: 20.0),
+          TextWidget(text: 'Address: '),
+          Text(_currentProfile.address, style: TextStyle(fontSize: 18.0)),
+          Divider(),
           Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          TextWidget(text: 'Body Mass Index: '),
-          SizedBox(width: 5.0),
-          Text(bmi.toStringAsFixed(3), style: TextStyle(fontSize: 18.0)),
-        ]),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextWidget(text: 'Age: '),
+              Text(age, style: TextStyle(fontSize: 18.0)),
+              SizedBox(width: 20.0),
+              TextWidget(text: 'Birthdate: '),
+              Text(formatBirthDate, style: TextStyle(fontSize: 18.0)),
+            ],
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextWidget(text: 'Gender :'),
+              SizedBox(width: 5.0),
+              Text(_currentProfile.gender, style: TextStyle(fontSize: 18.0)),
+              SizedBox(width: 20.0),
+              TextWidget(text: 'Civil Status:'),
+              SizedBox(width: 5.0),
+              Text(_currentProfile.civilStatus,
+                  style: TextStyle(fontSize: 18.0)),
+            ],
+          ),
+          Divider(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            TextWidget(text: 'Blood Type:'),
+            SizedBox(width: 5.0),
+            Text(_currentProfile.bloodType, style: TextStyle(fontSize: 18.0)),
+          ]),
+          Divider(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            TextWidget(text: 'Height:'),
+            SizedBox(width: 5.0),
+            Text(_currentProfile.height.toString() + ' m',
+                style: TextStyle(fontSize: 18.0)),
+            SizedBox(width: 20.0),
+            TextWidget(text: 'Weight:'),
+            SizedBox(width: 5.0),
+            Text(_currentProfile.weight.toString() + ' kg',
+                style: TextStyle(fontSize: 18.0)),
+          ]),
+          Divider(),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            TextWidget(text: 'Body Mass Index: '),
+            SizedBox(width: 5.0),
+            Text(bmi.toStringAsFixed(3), style: TextStyle(fontSize: 18.0)),
+          ]),
           Divider(),
           TextWidget(text: 'Category: '),
           SizedBox(width: 5.0),
           Text(bmiCategory, style: TextStyle(fontSize: 18.0)),
-          SizedBox(height:50),
-          RaisedButtonIcon(Icons.edit, 'Edit Profile', () {Navigator.popAndPushNamed(context, Strings.editProfilePageRoute);})
-      ]),
+          // SizedBox(height:50),
+          // RaisedButtonIcon(Icons.edit, 'Edit Profile', () {Navigator.popAndPushNamed(context, Strings.editProfilePageRoute);})
+        ]),
+      ),
     );
   }
 }
--- lib/widgets/edit_profile_widgets.dart
+++ lib/widgets/edit_profile_widgets.dart
@@ -2,71 +2,70 @@ import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
-Widget buildGender(context, currentValue, initialValue, state) {
+Widget buildGender(context, currentValue, state) {
   return Container(
-    width: 240,
-    child: DropdownButtonFormField<String>(
-    decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-    ),
-    items: [
-      DropdownMenuItem<String>(child: Text('Male'), value: 'Male'),
-      DropdownMenuItem<String>(child: Text('Female'), value: 'Female'),
-    ],
-    onChanged: state,
-    hint: Text(currentValue, style: TextStyle(color: Colors.black)),
-    value: initialValue,
-  ));
-}
-
-Widget buildCivilStatus(context,value, state) {
-  return Container(
-    width: 240,
-    child: DropdownButtonFormField<String>(
-    decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-    ),
-    items: [
-      DropdownMenuItem<String>(child: Text('Single'), value: 'Single'),
-      DropdownMenuItem<String>(child: Text('Married'), value: 'Married'),
-    ],
-    onChanged: state,
-    hint: Text('Civil Status'),
-    value: value,
-  ));
-}
-
-Widget buildBloodType(context, value, onChanged) {
-  return  Container(
-    width: 240,
-    child: DropdownButtonFormField<String>(
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'Please enter your bloodtype';
-          }
-          return null;
-        },
+      width: 240,
+      child: DropdownButtonFormField<String>(
         decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
         ),
         items: [
-          DropdownMenuItem<String>(child: Text('A+'), value: 'A+'),
-          DropdownMenuItem<String>(child: Text('A-'), value: 'A-'),
-          DropdownMenuItem<String>(child: Text('B+'), value: 'B+'),
-          DropdownMenuItem<String>(child: Text('B-'), value: 'B-'),
-          DropdownMenuItem<String>(child: Text('O+'), value: 'O+'),
-          DropdownMenuItem<String>(child: Text('O-'), value: 'O-'),
-          DropdownMenuItem<String>(child: Text('AB+'), value: 'AB+'),
-          DropdownMenuItem<String>(child: Text('AB-'), value: 'AB-'),
+          DropdownMenuItem<String>(child: Text('Male'), value: 'Male'),
+          DropdownMenuItem<String>(child: Text('Female'), value: 'Female'),
         ],
-        onChanged: onChanged,
-        hint: Text('Blood Type', style: TextStyle(fontSize: 15.0)),
+        onChanged: state,
+        hint: Text(currentValue, style: TextStyle(color: Colors.black)),
+        value: currentValue,
+      ));
+}
+
+Widget buildCivilStatus(context, value, state) {
+  return Container(
+      width: 240,
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+        items: [
+          DropdownMenuItem<String>(child: Text('Single'), value: 'Single'),
+          DropdownMenuItem<String>(child: Text('Married'), value: 'Married'),
+        ],
+        onChanged: state,
+        hint: Text('Civil Status'),
         value: value,
+      ));
+}
+
+Widget buildBloodType(context, value, onChanged) {
+  return Container(
+    width: 240,
+    child: DropdownButtonFormField<String>(
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter your bloodtype';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      items: [
+        DropdownMenuItem<String>(child: Text('A+'), value: 'A+'),
+        DropdownMenuItem<String>(child: Text('A-'), value: 'A-'),
+        DropdownMenuItem<String>(child: Text('B+'), value: 'B+'),
+        DropdownMenuItem<String>(child: Text('B-'), value: 'B-'),
+        DropdownMenuItem<String>(child: Text('O+'), value: 'O+'),
+        DropdownMenuItem<String>(child: Text('O-'), value: 'O-'),
+        DropdownMenuItem<String>(child: Text('AB+'), value: 'AB+'),
+        DropdownMenuItem<String>(child: Text('AB-'), value: 'AB-'),
+      ],
+      onChanged: onChanged,
+      hint: Text('Blood Type', style: TextStyle(fontSize: 15.0)),
+      value: value,
+    ),
   );
 }
 
@@ -95,44 +94,43 @@ Widget buildDescription(context, initialValue, description, onChanged) {
 }
 
 Widget buildTextFormField(context, String label, initialValue, onChanged) {
-    return Container(
-      width: 240,
-      child: TextFormField(
-          initialValue: initialValue,
-          decoration: InputDecoration(
-            labelText: label,
-            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-          ),
-          onChanged: onChanged
-      ),
-    );
+  return Container(
+    width: 240,
+    child: TextFormField(
+        initialValue: initialValue,
+        decoration: InputDecoration(
+          labelText: label,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+        onChanged: onChanged),
+  );
 }
 
 Widget buildDateTimePicker(context, initialValue, onChanged) {
-    final format = DateFormat("yMMMMd");
-    return Container(
+  final format = DateFormat("yMMMMd");
+  return Container(
     width: 240,
     child: DateTimeField(
-        initialValue: initialValue,
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-          labelText: 'Birthdate',
-          // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-        ),
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-            context: context,
-            firstDate:  DateTime(DateTime.now().year - 50),
-            lastDate: DateTime(DateTime.now().year + 50),
-            initialDate: currentValue ?? DateTime.now(),
-           );
-        },
-        onChanged: onChanged,
+      initialValue: initialValue,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        labelText: 'Birthdate',
+        // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
       ),
-    );
+      format: format,
+      onShowPicker: (context, currentValue) {
+        return showDatePicker(
+          context: context,
+          firstDate: DateTime(DateTime.now().year - 50),
+          lastDate: DateTime(DateTime.now().year + 50),
+          initialDate: currentValue ?? DateTime.now(),
+        );
+      },
+      onChanged: onChanged,
+    ),
+  );
 }
 
 Widget buildHeightWeightField(contex, controller, initialValue, suffix) {
@@ -144,9 +142,10 @@ Widget buildHeightWeightField(contex, controller, initialValue, suffix) {
       style: TextStyle(fontSize: 15),
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
-          hintText: initialValue, suffixText: suffix,
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        hintText: initialValue,
+        suffixText: suffix,
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     ),
   );
