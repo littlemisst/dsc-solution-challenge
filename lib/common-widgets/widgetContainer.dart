c03fda2c5bac2a974fcf8ab7e5a4ac516610297c
teofy <teofyrbns@gmail.com>
2020-04-23 21:58:51 +0800

    edit ui for profile

--- lib/common-widgets/widgetContainer.dart
+++ lib/common-widgets/widgetContainer.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 class ContentContainer extends StatelessWidget {
-  const ContentContainer({Key key, this.padding, this.child, this.width}) : super(key:key);
+  const ContentContainer({Key key, this.padding, this.child, this.width, this.margin}) : super(key:key);
   final EdgeInsets padding;
   final Widget child;
   final double width;
+  final EdgeInsets margin;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: margin,
       width: width,
       padding: padding,
       child: Material(
